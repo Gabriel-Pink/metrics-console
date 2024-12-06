5da35c7839c4d5c7050b8b56f55efc3f04b2f167
Gabriel Pink <aws@gabriel-rosa.com>
2024-12-05 22:05:14 -0300

    🔑 Cria novo diretório src/app/signin/ para telas de login

---
+++ src/app/signin/page.tsx
@@ -0,0 +1,13 @@
+import { SigninForm } from "@/Form/signin";
+
+export default function Signin() {
+  return (
+    <div className="h-screen w-screen flex justify-center items-center">
+        <div className="w-full">
+
+            <SigninForm />
+
+        </div>
+    </div>
+  );
+}
