400bde986ef4896ab8ae76cb2d623b0bfe8eee51
Gabriel Pink <aws@gabriel-rosa.com>
2024-12-11 01:56:49 -0300

    🎨 Alterações na página principal

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,16 @@
 import { ConnectionBox } from "@/components/connectionBox";
+import { WsConnection } from "@/components/wsConnection";
 import Image from "next/image";
 
 export default function Home() {
+
+  
+
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+    <div className="">
       
-      <div className="flex flex-col">
-        <ConnectionBox.Root></ConnectionBox.Root>
-        <ConnectionBox.Root></ConnectionBox.Root>
-        <ConnectionBox.Root></ConnectionBox.Root>
-        <ConnectionBox.Root></ConnectionBox.Root>
-        <ConnectionBox.Root></ConnectionBox.Root>
-      </div>
+
+      <WsConnection />
 
     </div>
   );
