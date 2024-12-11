4d903bbcd89cad6cc49b69d1cc60d574a104c58c
Gabriel Pink <aws@gabriel-rosa.com>
2024-12-11 01:58:23 -0300

    ✏️ Alterado index.tsx

--- src/components/connectionBox/index.tsx
+++ src/components/connectionBox/index.tsx
@@ -1,6 +1,12 @@
+import { ConnectionBoxBody } from "./connectionBoxBody";
+import { ConnectionBoxHeader } from "./connectionBoxHeader";
+import { ConnectionBoxList } from "./connectionBoxList";
 import { ConnectionBoxRoot } from "./connectionBoxRoot";
 
 export const ConnectionBox = {
     Root: ConnectionBoxRoot,
+    Header: ConnectionBoxHeader,
+    Body: ConnectionBoxBody,
+    List: ConnectionBoxList,
     
 }
