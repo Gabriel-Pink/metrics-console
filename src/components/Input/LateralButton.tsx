8c8efa707cf891bd329792f6ad17a2f34ed5a1fd
Gabriel Pink <aws@gabriel-rosa.com>
2024-12-11 01:58:10 -0300

    ✏️ Alterado LateralButton.tsx

--- src/components/Input/LateralButton.tsx
+++ src/components/Input/LateralButton.tsx
@@ -9,7 +9,7 @@ interface LateralButtonProps {
 
 export function LateralButton( { border = "rounded", Icon = CornerDownLeft }: LateralButtonProps ) {
     return (
-        <button className="bg-blue-500 text-white font-semibold py-2 px-2 rounded-lg shadow-md hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-300s absolute right-2 top-1/2 transform -translate-y-1/2">
+        <button className="bg-blue-500 text-white font-semibold py-2 px-2 shadow-md hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-300s absolute right-2 top-1/2 transform -translate-y-1/2 rounded-xl">
             <Icon size={20} />
         </button>
     );
