cf6da4b3d6a42d4d7e9981951fa1b19e2718632b
Gabriel Pink <aws@gabriel-rosa.com>
2024-12-11 01:58:17 -0300

    ✏️ Alterado connectionBoxRoot.tsx

--- src/components/connectionBox/connectionBoxRoot.tsx
+++ src/components/connectionBox/connectionBoxRoot.tsx
@@ -1,7 +1,15 @@
-export function ConnectionBoxRoot() {
+import { ReactNode } from "react";
+
+interface ConnectionBoxRootProrps {
+    children?: ReactNode
+}
+
+export function ConnectionBoxRoot ( { children }: ConnectionBoxRootProrps ) {
     return (
-        <div>
-            dfesafsdfs
+        <div className="border-1 border-gray-300 bg-[#fcfcfc] rounded shadow-md transition-all duration-300 text-sm">
+
+            { children }
+            
         </div>
     );
 }
